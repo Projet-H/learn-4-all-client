7686d0ee3fde7297e90b5c9d68e01857e162c8d7
Buchaillat Rémi <buchaillat.remi@hotmail.com>
2020-03-31 09:02:49 +0200

    end of login's design

--- src/components/register/registerSchema.js
+++ src/components/register/registerSchema.js
@@ -5,25 +5,25 @@ export const registerSchema = Yup.object().shape({
     .trim()
     .email("Votre email est invalide.")
     .max(100, "Votre email ne peut pas avoir plus de 100 caractères.")
-    .required("L'email est requis."),
+    .required("L'email est obligatoire."),
   username: Yup.string()
     .trim()
-    .required("Le nom d'utilisateur est requis.")
+    .required("Le nom d'utilisateur est obligatoire.")
     .min(5, "Votre nom d'utilisateur doit comporter minimum 5 caractères.")
     .max(50, "Votre nom d'utilisateur doit comporter maximum 50 caractères."),
   lastname: Yup.string()
     .trim()
-    .required("Le nom est requis.")
+    .required("Le nom est obligatoire.")
     .min(3, "Votre nom doit comporter minimum 3 caractères.")
     .max(50, "Votre nom doit comporter maximum 50 caractères."),
   firstname: Yup.string()
     .trim()
-    .required("Le prénom est requis.")
+    .required("Le prénom est obligatoire.")
     .min(3, "Votre prénom doit comporter minimum 3 caractères.")
     .max(50, "Votre prénom doit comporter maximum 50 caractères."),
   password: Yup.string()
     .trim()
-    .required("Le mot de passe est requis.")
+    .required("Le mot de passe est obligatoire.")
     .min(8, "Votre mot de passe doit contenir au minimum 8 caractères.")
     .matches(/[0-9]/, "Le mot de passe doit contenir au moins 1 chiffre.")
     .matches(
@@ -37,5 +37,5 @@ export const registerSchema = Yup.object().shape({
   confirmPassword: Yup.string()
     .trim()
     .oneOf([Yup.ref("password")], "Les mots de passe doivent correspondre.")
-    .required("Le mot de passe de confirmation est requis.")
+    .required("Le mot de passe de confirmation est obligatoire.")
 });
