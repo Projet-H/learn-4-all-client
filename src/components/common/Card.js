2060e4cda5704ef240829044360719a160c286c3
Buchaillat Rémi <buchaillat.remi@hotmail.com>
2020-03-31 11:33:44 +0200

    design of class's list

---
+++ src/components/common/Card.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { makeStyles, Grid, Button } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  list: {
+    display: "flex",
+    flexGrow: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 16,
+    boxShadow: "0 0 0 1px rgba(63,63,68,0.05), 0 1px 3px 0 rgba(63,63,68,0.15)",
+    backgroundColor: "#FFF",
+    borderRadius: 4,
+    marginBottom: 16
+  },
+  element: {
+    padding: 8
+  },
+  mainTitle: {
+    color: "#263238",
+    fontSize: 17,
+    fontWeight: 600,
+    lineHeight: "20px",
+    letterSpacing: "-0.05px"
+  },
+  title: {
+    color: "#263238",
+    fontSize: 15,
+    fontWeight: 600,
+    lineHeight: "20px",
+    letterSpacing: "-0.05px"
+  },
+  subtitle: {
+    color: "#546e7a",
+    fontSize: 13,
+    fontWeight: 400,
+    lineHeight: "18px",
+    letterSpacing: "-0.04px"
+  }
+});
+
+export const Card = ({ value }) => {
+  const classes = useStyles();
+  const { push } = useHistory();
+
+  return (
+    <Grid container className={classes.list}>
+      <Grid item xs={6} sm={4} className={classes.element}>
+        <div className={classes.mainTitle}>{value.name}</div>
+      </Grid>
+      <Grid item xs={6} sm={3} className={classes.element}>
+        <div className={classes.title}>54</div>
+        <div className={classes.subtitle}>Nombre de matières</div>
+      </Grid>
+      <Grid item xs={6} sm={3} className={classes.element}>
+        <div className={classes.title}>{value.createdAt}</div>
+        <div className={classes.subtitle}>Date de création</div>
+      </Grid>
+      <Grid item xs={6} sm={2} className={classes.element}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => push(`${value.slug}/subject`)}
+        >
+          Voir
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
